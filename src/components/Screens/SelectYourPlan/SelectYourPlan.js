af0ae14a05eabaac5777a1f8376ffb8adce8b396
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2023-01-10 16:05:52 +0200

    Implement desktop responsiveness for screens

--- src/components/Screens/SelectYourPlan/SelectYourPlan.js
+++ src/components/Screens/SelectYourPlan/SelectYourPlan.js
@@ -4,6 +4,7 @@ import FooterNavigation from '../../Navigation/FooterNavigation';
 import PlanCheckbox from './PlanCheckbox';
 import MonthyYearlyCheckbox from './MonthlyYearlyCheckbox';
 import MainContext from '../../../store/main-context';
+import classes from './SelectYourPlan.module.css';
 
 const SelectYourPlan = () => {
   const ctx = useContext(MainContext);
@@ -15,18 +16,20 @@ const SelectYourPlan = () => {
         title='Select your plan'
         description='You have the option of monthly or yearly billing.'
       >
-        {plans.map((plan) => (
-          <PlanCheckbox
-            key={plan.id}
-            id={plan.id}
-            title={plan.title}
-            image={plan.image}
-            isActive={plan.isActive}
-            monthlyPrice={plan.monthlyPrice}
-            yearlyPrice={plan.yearlyPrice}
-            yearlyFreeMonths={plan.yearlyFreeMonths}
-          />
-        ))}
+        <div className={classes.plans}>
+          {plans.map((plan) => (
+            <PlanCheckbox
+              key={plan.id}
+              id={plan.id}
+              title={plan.title}
+              image={plan.image}
+              isActive={plan.isActive}
+              monthlyPrice={plan.monthlyPrice}
+              yearlyPrice={plan.yearlyPrice}
+              yearlyFreeMonths={plan.yearlyFreeMonths}
+            />
+          ))}
+        </div>
         <MonthyYearlyCheckbox />
       </Card>
       <FooterNavigation />
